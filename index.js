a0ef7c56c2bbf1a734cab4394d58dceb0911c529
Sanyam Vikram <sanyamv@google.com>
2023-09-10 18:36:48 +0000

    Added logic for SBOM Generation

--- index.js
+++ index.js
@@ -1,16 +1,48 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
+const exec = require('@actions/exec');
+const { Storage } = require('@google-cloud/storage');
+const fetch = require('node-fetch');
 
-try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
+async function run() {
+  try {
+    // Set up GCS credentials
+    const storage = new Storage({
+      keyFilename: 'gcs-crdentials.json'
+    });
+
+    // Set the GCS bucket and file name
+    const bucketName = 'sca_github_action';
+    const fileName = 'dependency-graph.json';
+
+    // Fetch the dependency graph using the GitHub API
+    const githubToken = process.env.GITHUB_TOKEN; // GitHub Token is automatically provided in Actions
+
+    const response = await fetch('https://api.github.com/repos/yourusername/yourrepository/dependency-graph', {
+      headers: {
+        Authorization: `Bearer ${githubToken}`,
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(`Failed to fetch dependency graph: ${response.statusText}`);
+    }
+
+    const dependencyGraph = await response.json();
+
+    // Save the dependency graph to a file (adjust this part as needed)
+    const fs = require('fs');
+    fs.writeFileSync(fileName, JSON.stringify(dependencyGraph, null, 2));
+
+    // Upload the file to GCS
+    await storage.bucket(bucketName).upload(fileName);
+
+    // Optional: Delete the local file if needed
+    // fs.unlinkSync(fileName);
+
+    core.setOutput('uploaded-file', fileName);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
 
+run();
