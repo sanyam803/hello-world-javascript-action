a14b3f4ca55c7747c4efe8cf1938b902b7d49891
Sanyam Vikram <sanyam.vikram1@gmail.com>
2023-09-11 18:24:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -23,22 +23,23 @@ async function run() {
     } = await octokit.rest.users.getAuthenticated();
     console.log("Hello, %s", login);
     core.info("Hello, %s", login);
+    
     console.log("successfully generated octokit client");
     
     // Set the GCS bucket and file name
-    const bucketName = 'sca_github_action';
-    const fileName = 'dependency-graph.json';
+    // const bucketName = 'sca_github_action';
+    // const fileName = 'dependency-graph.json';
 
-    // Fetch the dependency graph using the GitHub API
-    const githubToken = process.env.GITHUB_TOKEN; // GitHub Token is automatically provided in Actions
+    // // Fetch the dependency graph using the GitHub API
+    // const githubToken = process.env.GITHUB_TOKEN; // GitHub Token is automatically provided in Actions
 
-    const response  = await octokit.request('GET /repos/sanyam803/Neural-Image-Synthesis/dependency-graph/sbom', {
-           owner: 'sanyam803',
-           repo: 'Neural-Image-Synthesis',
-           headers: {
-               'X-GitHub-Api-Version': '2022-11-28'
-           }
-    })
+    // const response  = await octokit.request('GET /repos/sanyam803/Neural-Image-Synthesis/dependency-graph/sbom', {
+    //        owner: 'sanyam803',
+    //        repo: 'Neural-Image-Synthesis',
+    //        headers: {
+    //            'X-GitHub-Api-Version': '2022-11-28'
+    //        }
+    // })
 
     // const response = await fetch('https://api.github.com/repos/sanyam803/Neural-Image-Synthesis/dependency-graph', {
     //   headers: {
@@ -52,19 +53,19 @@ async function run() {
     //   throw new Error(`Failed to fetch dependency graph: ${response.statusText}`);
     // }
 
-    const dependencyGraph = response.sbom;
+    // const dependencyGraph = response.sbom;
 
-    // Save the dependency graph to a file (adjust this part as needed)
-    const fs = require('fs');
-    fs.writeFileSync(fileName, JSON.stringify(dependencyGraph, null, 2));
+    // // Save the dependency graph to a file (adjust this part as needed)
+    // const fs = require('fs');
+    // fs.writeFileSync(fileName, JSON.stringify(dependencyGraph, null, 2));
 
-    // Upload the file to GCS
-    await storage.bucket(bucketName).upload(fileName);
+    // // Upload the file to GCS
+    // await storage.bucket(bucketName).upload(fileName);
 
-    // Optional: Delete the local file if needed
-    // fs.unlinkSync(fileName);
+    // // Optional: Delete the local file if needed
+    // // fs.unlinkSync(fileName);
 
-    core.setOutput('uploaded-file', fileName);
+    // core.setOutput('uploaded-file', fileName);
   } catch (error) {
     core.setFailed(error.message);
   }
